2770fcaf13b6b9625918283e077937c80d2870d7
NullDev (Shadow) <NL-Dev@yandex.com>
2017-11-09 11:35:30 +0100

    Added message handling and object search

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var Discord = require("discord.js");
-var fs      = require('fs');
+var fs      = require("fs");
 
 ////////////////////////////////
 //----------------------------//
@@ -9,22 +9,35 @@ var fs      = require('fs');
 //----------------------------//
 ////////////////////////////////
 
-require.extensions['.json'] = function (module, filename) { module.exports = fs.readFileSync(filename, 'utf8'); };
+var noop = () => {};
+
+require.extensions['.json'] = function (module, filename) { module.exports = fs.readFileSync(filename, "utf8"); };
 var jsondata = require('./config.json');
 var raw      = JSON.parse(jsondata);
 
 var token = raw.bot.token;
 
 function messageHandle(msg){
-    console.log(msg);
+    if ((msg.author.username).toLowerCase() == "impulse"){
+        var q = (msg.content).toLowerCase();
+        var a = raw.answers;
+
+        for (var key in a){
+            var exp = new RegExp(escapeRegEx(key), "gi");
+            if (a.hasOwnProperty(key)) if (q.match(exp)) msg.reply(a[key]);
+        }
+    }
+    else return;
 }
+
+function escapeRegEx(str) { return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"); }
 
 var client = new class Client extends Discord.Client {
     constructor() {
         super({ fetchAllMembers: true });
 
-        this.on('ready', () => { console.log("Started.") });
-        this.on('message', message => { messageHandle(message); });
+        this.on("ready", () => { console.log(`Logged in as ${client.user.tag}!`) });
+        this.on("message", message => { messageHandle(message); });
 
         this.login(token);
     }
